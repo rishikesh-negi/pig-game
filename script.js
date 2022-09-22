ed0834e79b9aa5ef3ec7fe6f54c7eb8170af7c66
Rishikesh Negi <rishinegi97@gmail.com>
2022-09-22 19:50:38 +0530

    Final Commit

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ btnNew.addEventListener('click', function () {
     playerNames[0].textContent = 'Player 1';
     playerNames[1].textContent = 'Player 2';
     btnRoll.classList.remove('hidden');
-    btnHold.classList.remove('hidden');
+    btnHold.classList.add('hidden');
     dice.classList.add('hidden');
   }
 });
